00441b6121fcd26c3fc434a6ede29cec4eaea990
jacopodl <jacopo.delu@gmail.com>
2024-07-01 15:54:43 +0200

    wip: compressor module

---
+++ compressor/compressor.cpp
@@ -0,0 +1,141 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#include <argon/vm/runtime.h>
+
+#include <version.h>
+
+#include <compressor/compressor.h>
+
+using namespace argon::vm::datatype;
+using namespace arlib::compressor;
+
+const FunctionDef compressor_methods[] = {
+        ARGON_METHOD_STUB("compress",
+                          "Compress data incrementally.\n"
+                          "\n"
+                          "Provide data to the compressor object.\n"
+                          "When you have finished providing data to the compressor, call the flush() method to finish "
+                          "the compression process.\n"
+                          "\n"
+                          "- Parameter data: Bufferable object containing the data to compress.\n"
+                          "- Returns: A bytes object containing compressed data.\n",
+                          ": data", false, true),
+        ARGON_METHOD_STUB("flush",
+                          "Finish the compression process.\n"
+                          "\n"
+                          "Returns a bytes object containing any remaining compressed data. You must not use the compressor "
+                          "object after calling this method. This method may be called more than once. Subsequent calls will return empty bytes objects.\n"
+                          "\n"
+                          "- Returns: A bytes object containing any remaining compressed data.\n",
+                          nullptr, false, true),
+        ARGON_METHOD_SENTINEL
+};
+
+const ObjectSlots compressor_objslot = {
+        compressor_methods,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        -1
+};
+
+TypeInfo CompressorType = {
+        AROBJ_HEAD_INIT_TYPE,
+        "Compressor",
+        nullptr,
+        nullptr,
+        0,
+        TypeInfoFlags::TRAIT,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        &compressor_objslot,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr
+};
+const TypeInfo *arlib::compressor::type_compressor_t_ = &CompressorType;
+
+const FunctionDef decompressor_methods[] = {
+        ARGON_METHOD_STUB("decompress",
+                          "",
+                          nullptr, false, true),
+        ARGON_METHOD_SENTINEL
+};
+
+const ObjectSlots decompressor_objslot = {
+        decompressor_methods,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        -1
+};
+
+TypeInfo DecompressorType = {
+        AROBJ_HEAD_INIT_TYPE,
+        "Decompressor",
+        nullptr,
+        nullptr,
+        0,
+        TypeInfoFlags::TRAIT,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr,
+        &decompressor_objslot,
+        nullptr,
+        nullptr,
+        nullptr,
+        nullptr
+};
+const TypeInfo *arlib::compressor::type_decompressor_t_ = &DecompressorType;
+
+const ModuleEntry compressor_entries[] = {
+        MODULE_EXPORT_TYPE(type_compressor_t_),
+        MODULE_EXPORT_TYPE(type_decompressor_t_),
+
+        ARGON_MODULE_SENTINEL
+};
+
+bool CompressorInit(Module *self) {
+    if(!TypeInit((TypeInfo*)type_compressor_t_, nullptr))
+        return false;
+
+    if(!TypeInit((TypeInfo*)type_decompressor_t_, nullptr))
+        return false;
+
+    return true;
+}
+
+constexpr ModuleInit ModuleCompressor = {
+        "compressor",
+        "",
+        ARLIB_VERSION,
+        compressor_entries,
+        CompressorInit,
+        nullptr
+};
+
+ARGON_MODULE_INIT(ModuleCompressor)
---
+++ compressor/compressor.h
@@ -0,0 +1,17 @@
+// This source file is part of the Argon project.
+//
+// Licensed under the Apache License v2.0
+
+#ifndef ARLIB_COMPRESSOR_COMPRESSOR_H_
+#define ARLIB_COMPRESSOR_COMPRESSOR_H_
+
+#include <argon/util/macros.h>
+
+#include <argon/vm/datatype/arobject.h>
+
+namespace arlib::compressor {
+    extern const argon::vm::datatype::TypeInfo *type_compressor_t_;
+    extern const argon::vm::datatype::TypeInfo *type_decompressor_t_;
+} // namespace arlib::compressor
+
+#endif // !ARLIB_COMPRESSOR_COMPRESSOR_H_
